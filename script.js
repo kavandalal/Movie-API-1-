c36b5f11e57dddc121b07baffe0b8b780986e3b0
kavandalal <kavandalal003@gmail.com>
2021-10-02 15:55:24 +0530

    1st update

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 const fetchData = (name) => {
-  var i = {};
-  i = fetch(
-    `https://imdb8.p.rapidapi.com/auto-complete?q=${name}`, {
-  	"method": "GET",
-  	"headers": {
-  		"x-rapidapi-key": "4573afb52amsh7e36d630c1420fbp1b62a6jsn90e72f7f75f7",
-  		"x-rapidapi-host": "imdb8.p.rapidapi.com"
-  	}
-  })
-  .then(response => {
-    return response.json();
-  })
-    .then(data => {
-      console.log(data);
-      const html = data.d.map(single => {
-        return `
-        <div style="display:flex;flex-direction:column">
-          <div>
-            <img src="${single.i.imageUrl}" height="300px" width:"500px" style="object-fit:cover"/>
+	var i = {};
+	var api_key = '743d091913d773e39a79b71be11164b2',
+		i = fetch(
+			`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${name}`,
+			{
+				method: 'GET',
+			}
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data.results);
+				const html = data.results
+					.map((single) => {
+						return `
+        <div class='ctr'>
+          <div class='image'>
+            <img src= 'https://image.tmdb.org/t/p/w500${single.poster_path}'/>
           </div>
-          <div>
-            <h2 class="name">${single.l}</h2>
-            <h3 class="type">${single.q}</h3>
-            <p class="desc">Rank : ${single.rank}</p>
-            <p class="desc">Actor : ${single.s}</p>
-            <p class="desc">Years : ${single.yr || single.y}</p>
+          <div class="ctr2">
+            <div class='heading'>
+              <h2 class="name">${single.title}</h2>
+            </div>
+            <div class='info'>
+              <p class="rate">Rating : ${single.vote_average}</p>
+              <p class="rel">Release Date : ${single.release_date}
+              </p>
+              <p class="desc">Overview : ${single.overview}</p>
+            </div>
           </div>
-
         </div>
-        `
-      }).join('');
-      document.querySelector('#body').insertAdjacentHTML('beforeend', html);
-    }
-    
-  )
-  .catch(err => {
-  	console.error(err);
-  });
-}
+        `;
+					})
+					.join('');
+				document.querySelector('#body').insertAdjacentHTML('beforeend', html);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+};
 
 const submitButton = () => {
-  // console.log('hello');
-  document.getElementById('body').innerHTML = '';
-  var x = document.getElementById('search').value;
-  console.log(x);
-  fetchData(x.split(" ").join('%20'));
-}
+	// console.log('hello');
+	document.getElementById('body').innerHTML = '';
+	var x = document.getElementById('search').value;
+	console.log(x);
+	fetchData(x.split(' ').join('%20'));
+};
+
+document.getElementById('search').addEventListener('keyup', (key) => {
+	if (key.keyCode === 13) {
+		//13 == ENTER key
+		document.getElementById('sub-btn').click();
+	}
+});
